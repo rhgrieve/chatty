a0e3e68ff54da65fef01143902b2e45b60fe6603
Harrison Grieve <rhg@alkmi.io>
2022-03-18 02:50:54 +0100

    chore: refactor

--- server.ts
+++ server.ts
@@ -16,10 +16,56 @@ class ChatUser {
   }
 }
 
-type ConnPool = {
-  [key: string]: ChatUser;
-};
-const connectionPool: ConnPool = {};
+class ConnPool {
+  connections: { [key: string]: ChatUser };
+
+  constructor() {
+    this.connections = {};
+  }
+
+  getConnectionByID(id: string): ChatUser | null {
+    return this.connections[id];
+  }
+
+  addConnection(user: ChatUser) {
+    this.connections[user.uuid] = user;
+    return user;
+  }
+
+  getActiveCount() {
+    return Object.keys(this.connections).filter((key) =>
+      this.connections[key].conn.readyState === 1
+    ).length;
+  }
+
+  getAllConnections() {
+    return Object.keys(this.connections).map((key) => this.connections[key]);
+  }
+}
+
+const connectionPool = new ConnPool();
+
+function onMessage(this: WebSocket, e: MessageEvent<any>): any {
+  console.log("socket message:", e.data);
+
+  const { id, message, username, datetime } = JSON.parse(e.data);
+  let sendingUser = connectionPool.getConnectionByID(id);
+  if (!sendingUser) {
+    sendingUser = connectionPool.addConnection(
+      new ChatUser(id, username, this),
+    );
+  }
+
+  connectionPool.getAllConnections().forEach((user) => {
+    user.conn.readyState === 1 && user.conn.send(JSON.stringify({
+      datetime,
+      username,
+      id,
+      message,
+      color: user.color,
+    }));
+  });
+}
 
 async function handle(conn: Deno.Conn) {
   const httpConn = Deno.serveHttp(conn);
@@ -37,26 +83,9 @@ function handleReq(req: Request): Response {
   socket.onopen = () => {
     console.log("socket opened");
   };
-  socket.onmessage = (e) => {
-    console.log("socket message:", e.data);
-    const { id, message, username, datetime } = JSON.parse(e.data);
-    const userExists = connectionPool[id];
-    if (!userExists) {
-      connectionPool[id] = new ChatUser(id, username, socket);
-    }
-    Object.keys(connectionPool).forEach((id) => {
-      const user = connectionPool[id];
-      user.conn.send(
-        JSON.stringify({
-          datetime,
-          username,
-          id,
-          message,
-          color: user.color,
-        }),
-      );
-    });
-  };
+
+  socket.onmessage = onMessage;
+
   socket.onerror = (e) =>
     console.log("socket error:", (<ErrorEvent> e).message);
   socket.onclose = () => console.log("socket closed");
